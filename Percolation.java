699f9e71b167a570f3f46efc7652cce901df35b5
RunicSyntax <107773663+RunicSyntax@users.noreply.github.com>
2023-09-05 13:21:50 +0000

    Comments moved

--- Percolation.java
+++ Percolation.java
@@ -1,24 +1,35 @@
 public class Percolation {
     public Percolation(int N) {
+    // create N-by-N grid, with all sites initially blocked
 
-    }; // create N-by-N grid, with all sites initially blocked
+    }; 
     public void open(int row, int col) {
+    // open the site (row, col) if it is not open already
 
-    }; // open the site (row, col) if it is not open already
+    }; 
     public boolean isOpen(int row, int col) {
-        return true;
-    }; // is the site (row, col) open?
-    public boolean isFull(int row, int col) {
-        return true;
-    }; // is the site (row, col) full?
-    public int numberOfOpenSites() {
-        return 1;
-    }; // number of open sites
-    public boolean percolates() {
-        return true;
-    }; // does the system percolate?
-    public static void main(String[] args) {
+    // is the site (row, col) open?
 
-    }; // unit testing (required)
+        return true;
+    }; 
+    public boolean isFull(int row, int col) {
+    // is the site (row, col) full?
+
+        return true;
+    }; 
+    public int numberOfOpenSites() {
+    // number of open sites
+
+        return 1;
+    }; 
+    public boolean percolates() {
+    // does the system percolate?
+
+        return true;
+    }; 
+    public static void main(String[] args) {
+    // unit testing (required)
+
+    }; 
 }
 
