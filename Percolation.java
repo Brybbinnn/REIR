78965b3d6786301ba198e8ad3a2b9172957a3e48
Bryndís Gunnarsdóttir <bryndis@programm.is>
2023-09-05 13:16:58 +0000

    m

--- Percolation.java
+++ Percolation.java
@@ -1,11 +1,24 @@
-public class Perculation {
-    public Percolation(int N) // create N-by-N grid, with all sites initially blocked
-    public void open(int row, int col) // open the site (row, col) if it is not open already
-    public boolean isOpen(int row, int col) // is the site (row, col) open?
-    public boolean isFull(int row, int col) // is the site (row, col) full?
-    public int numberOfOpenSites() // number of open sites
-    public boolean percolates() // does the system percolate?
-    public static void main(String[] args) // unit testing (required)
-    
-    }
+public class Percolation {
+    public Percolation(int N) {
+
+    }; // create N-by-N grid, with all sites initially blocked
+    public void open(int row, int col) {
+
+    }; // open the site (row, col) if it is not open already
+    public boolean isOpen(int row, int col) {
+        return true;
+    }; // is the site (row, col) open?
+    public boolean isFull(int row, int col) {
+        return true;
+    }; // is the site (row, col) full?
+    public int numberOfOpenSites() {
+        return 1;
+    }; // number of open sites
+    public boolean percolates() {
+        return true;
+    }; // does the system percolate?
+    public static void main(String[] args) {
+
+    }; // unit testing (required)
+}
 
