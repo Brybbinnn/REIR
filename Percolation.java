a3214b36dce888ee5bdff70e3e8c8f3aa2088ca5
RunicSyntax <107773663+RunicSyntax@users.noreply.github.com>
2023-09-14 13:35:05 +0000

    Global var added

--- Percolation.java
+++ Percolation.java
@@ -1,23 +1,29 @@
-import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+//import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    public static int grid[][];
+    public static int open = 1;
+    public static int closed = 0;
+
     public Percolation(int N) {
     // create N-by-N grid, with all sites initially blocked
     //WeightedQuickUnionUF wqu = new WeightedQuickUnionUF(N);
     //wqu.union(N, N);
-        int grid[][];
         grid = new int[N][N];
 
         for(int i = 0; i < N; i++){
             String gridOut = "";
             for(int j = 0; j < N; j++){
-                gridOut = gridOut.concat(Integer.toString(grid[i][j]));
+                gridOut = gridOut.concat(Integer.toString(grid[i][j]) + " ");
             }
             System.out.println(gridOut); 
         }
     }; 
     public void open(int row, int col) {
     // open the site (row, col) if it is not open already
+        if(grid[row][col] == 0){
+
+        }
 
     }; 
     public boolean isOpen(int row, int col) {
