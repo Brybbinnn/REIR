450fd1373163f41dc59873589c4e6e9b3a98943e
Bryndís Gunnarsdóttir <bryndis@programm.is>
2023-09-14 22:02:56 +0000

    kms p2

--- PercolationStats.java
+++ PercolationStats.java
@@ -1,11 +1,21 @@
-import edu.princeton.cs.algs4.StdStats;
+// import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class PercolationStats {
+    public static int N;
+    public static int T;
+
     public PercolationStats(int N, int T){
         // perform T independent experiments on an N-by-N grid
         if (N <= 0 || T <= 0) {
         throw new IllegalArgumentException();
         }
+        for (int i = 0; i < T; i++) {
+            i = StdRandom.uniformInt(N);
+            
+        }
+
+        
     }
 
     public double mean(){
@@ -14,5 +24,9 @@ public class PercolationStats {
 
     public double stddev(){
         return 0;
+    }
+
+    public static void main(String[] args) {
+        PercolationStats per = new PercolationStats(10, 10);
     }
 }
