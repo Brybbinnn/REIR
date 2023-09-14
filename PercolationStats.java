eca691feaf5534907d70f1cc262c6afb6d0b22fd
Bryndís Gunnarsdóttir <bryndis@programm.is>
2023-09-14 22:23:41 +0000

    maybe

--- PercolationStats.java
+++ PercolationStats.java
@@ -1,21 +1,26 @@
 // import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.Stopwatch;
 
 public class PercolationStats {
     public static int N;
     public static int T;
+    public static int x;
+    public static int y;
 
     public PercolationStats(int N, int T){
         // perform T independent experiments on an N-by-N grid
         if (N <= 0 || T <= 0) {
-        throw new IllegalArgumentException();
+            throw new IllegalArgumentException();
         }
-        for (int i = 0; i < T; i++) {
-            i = StdRandom.uniformInt(N);
-            
-        }
-
-        
+        Percolation p = new Percolation(N);
+        while (p.percolates() == false)
+            for (int i = 0; i < T; i++) {
+                x = StdRandom.uniformInt(N-1);
+                y = StdRandom.uniformInt(N-1);
+                p.open(x, y);
+            } 
+        System.out.println(p.numberOfOpenSites());
     }
 
     public double mean(){
@@ -27,6 +32,6 @@ public class PercolationStats {
     }
 
     public static void main(String[] args) {
-        PercolationStats per = new PercolationStats(10, 10);
+        PercolationStats per = new PercolationStats(3, 10000);
     }
 }
