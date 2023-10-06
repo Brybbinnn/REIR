9166cd79d3358342c4bdad44041b4ecc1ccf1075
RunicSyntax <107773663+RunicSyntax@users.noreply.github.com>
2023-10-06 15:17:29 +0000

    Added testing capabilities
    Added testing capabilities as well as changed segments to be partitioned into segments (to fulfill gradescope) and find_segments

--- S2/src/Fast.java
+++ S2/src/Fast.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Out;
 import edu.princeton.cs.algs4.Queue;
 
 public class Fast {
@@ -11,7 +13,7 @@ public class Fast {
         Arrays.sort(this.points);
     }
 
-    public Queue<Double> segments() {
+    public void find_segments() {
         for (Point p : this.points) {
             Queue<Double> segment = new Queue<>();
             for (Point q : this.points) {
@@ -20,6 +22,32 @@ public class Fast {
                     segment.enqueue(p.slopeTo(q));
                 }
                 // Arrays.sort(segment)
+            }
+            queue.enqueue(segment);
+        }
+    }
+
+    public Queue<Queue<Double>> segments() {
+        return this.queue;
+    }
+
+    public static void main(String[] args) {
+        In in = new In();
+        Out out = new Out();
+        int n = in.readInt();
+
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt(), y = in.readInt();
+            points[i] = new Point(x, y);
+
+        }
+        Fast fast = new Fast(points);
+        out.printf("Brute method...\n");
+        for (Queue<Double> q : fast.queue) {
+            out.printf("\n");
+            for (Double p : q) {
+                out.printf(p.toString() + ", ");
             }
         }
     }
