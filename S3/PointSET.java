54e16d5bf6cfa9739a90ea8b567c661aaaa2e43b
RunicSyntax <107773663+RunicSyntax@users.noreply.github.com>
2023-10-25 16:27:05 +0000

    Function implementations

--- S3/PointSET.java
+++ S3/PointSET.java
@@ -32,25 +32,33 @@ public class PointSET {
 
     // number of points in the set
     public int size() {
-        return 0;
+        return set.size();
     }
 
     // add the point p to the set (if it is not already in the set)
     public void insert(Point2D p) {
+        set.add(p);
     }
 
     // does the set contain the point p?
     public boolean contains(Point2D p) {
-        return false;
+        return set.contains(p);
     }
 
     // draw all of the points to standard draw
     public void draw() {
+        for (Point2D p : set) {
+            p.draw();
+        }
     }
 
     // all points in the set that are inside the rectangle
     public Iterable<Point2D> range(RectHV rect) {
-        return null;
+        for (Point2D point2d : set) {
+            if(rect.contains(point2d)){
+
+            }
+        }
     }
 
     // a nearest neighbor in the set to p; null if set is empty
